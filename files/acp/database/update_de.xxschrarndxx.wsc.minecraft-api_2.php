1f340c10783f17dd2e6248d828dc8682ed0ca45a
xXSchrandXx <marvinschrand@yahoo.de>
2022-10-17 17:33:19 +0200

    Update update_de.xxschrarndxx.wsc.minecraft-api_2.php

--- files/acp/database/update_de.xxschrarndxx.wsc.minecraft-api_2.php
+++ files/acp/database/update_de.xxschrarndxx.wsc.minecraft-api_2.php
@@ -20,17 +20,28 @@ foreach ($minecrafts as $minecraft) {
     try {
         $authEncoded = Base64::decode($minecraft->auth);
     } catch (RangeException $e) {
+        // Should never happen
+        \wcf\functions\exception\logThrowable($e);
         continue;
     } catch (TypeError $e) {
+        // Should never happen
+        \wcf\functions\exception\logThrowable($e);
         continue;
     }
     [$user, $password] = \explode(':', $authEncoded, 2);
+    $this->updateData($editor, $user, $password, $algorithmName, $algorithm);
+}
+
+function updateData($editor, $user, $password, $algorithmName, $algorithm, $i = 0)
+{
     try {
         $editor->update([
             'user' => $user,
             'password' => $algorithmName . ':' . $algorithm->hash($password)
         ]);
     } catch (DatabaseException $e) {
+        $i = $i++;
+        updateData($editor, $user . $i, $password, $algorithmName, $algorithm, $i);
         \wcf\functions\exception\logThrowable($e);
     }
 }
