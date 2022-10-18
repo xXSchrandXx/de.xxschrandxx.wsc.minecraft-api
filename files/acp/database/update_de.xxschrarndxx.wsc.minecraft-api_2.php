01475d0fdb1166945bf2eb2c39da65fcb8e2da09
xXSchrandXx <marvinschrand@yahoo.de>
2022-10-18 14:52:58 +0200

    Remove catches

--- files/acp/database/update_de.xxschrarndxx.wsc.minecraft-api_2.php
+++ files/acp/database/update_de.xxschrarndxx.wsc.minecraft-api_2.php
@@ -3,7 +3,6 @@
 use ParagonIE\ConstantTime\Base64;
 use wcf\data\minecraft\MinecraftEditor;
 use wcf\data\minecraft\MinecraftList;
-use wcf\system\database\exception\DatabaseException;
 use wcf\system\user\authentication\password\PasswordAlgorithmManager;
 
 $manager = PasswordAlgorithmManager::getInstance();
@@ -17,31 +16,10 @@ $minecrafts = $minecraftList->getObjects();
 foreach ($minecrafts as $minecraft) {
     $editor = new MinecraftEditor($minecraft);
     $algorithmName = PasswordAlgorithmManager::getInstance()->getNameFromAlgorithm($algorithm);
-    try {
-        $authEncoded = Base64::decode($minecraft->auth);
-    } catch (RangeException $e) {
-        // Should never happen
-        \wcf\functions\exception\logThrowable($e);
-        continue;
-    } catch (TypeError $e) {
-        // Should never happen
-        \wcf\functions\exception\logThrowable($e);
-        continue;
-    }
+    $authEncoded = Base64::decode($minecraft->auth);
     [$user, $password] = \explode(':', $authEncoded, 2);
-    $this->updateData($editor, $user, $password, $algorithmName, $algorithm);
-}
-
-function updateData($editor, $user, $password, $algorithmName, $algorithm, $i = 0)
-{
-    try {
-        $editor->update([
-            'user' => $user,
-            'password' => $algorithmName . ':' . $algorithm->hash($password)
-        ]);
-    } catch (DatabaseException $e) {
-        $i = $i++;
-        updateData($editor, $user . $i, $password, $algorithmName, $algorithm, $i);
-        \wcf\functions\exception\logThrowable($e);
-    }
+    $editor->update([
+        'user' => $user,
+        'password' => $algorithmName . ':' . $algorithm->hash($password)
+    ]);
 }
